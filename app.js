7324f09fe4e8edeb5bf64f9d70909597ee0e7d11
RiyaMehta1 <riyahmehta345@gmail.com>
2026-05-12 10:08:22 +0530

    Node.jsCode

---
+++ app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const port = process.env.PORT;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hello from Elastic Beanstalk Working App');
+});
+
+server.listen(port);
